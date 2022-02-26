8c29b1651aa4514686be56ca01562e982753c650
SMac-Dev <samuelm@realtimetraffic.com.au>
2022-02-26 16:44:39 +1100

    stuff that half works

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 # import required libraries for program
-from fastapi import FastAPI, Request
+from fileinput import filename
+from fastapi import FastAPI, Request, BackgroundTasks
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
+from functions.linear_reg import Ml
+import pandas as pd
 
 # import pandas as pd
 # from sklearn.datasets import load_files
@@ -33,6 +36,26 @@ def tutorial(request: Request):
 @app.get('/train')
 def train(request: Request):
     return templates.TemplateResponse('train.html', {'request': request})
+
+@app.post('/train')
+async def train(request: Request, model: str, filename:str, train_size:float, target: str,background_tasks : BackgroundTasks):
+    print("Filename: ", filename)
+    print("Train_size Type: ", type(train_size))
+    load = Ml(filename=filename)
+    df = load.read_csv()
+    print(df)
+    if not df:
+        print("Error Reading file. Please check the name of the file.")
+    else:
+        ml = Ml(dataframe=df,target_column=target)
+        ml.load_x_y()
+        print(ml.x,ml.y)
+        if ml.x != 0 and ml.y != 0:
+           background_tasks.add_task(ml.training())
+           return templates.TemplateResponse('train.html', {'request': request})
+        else:
+            print("Dataset doesn't have the target feature.")
+
 
 # Predict page
 @app.get('/predict')
